17cf437fd7fde579f0786086d92ef00da2596997
manugeo <manugeo13@gmail.com>
2019-08-26 14:15:20 +0530

    Completing the game.

--- src/index.js
+++ src/index.js
@@ -2,35 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-      super(props);
-
-      this.state = {
-          value: null
-      };
-
-  }
-
-  render() {
-    return (
-      <button 
-        className="square"
-        onClick={ () => this.setState({value: 'X'}) }
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: [null, null, null, null, null, null, null, null, null],
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const {xIsNext} = this.state;
+    const squares = this.state.squares.slice();
+
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? 'X' : 'O';
+    this.setState({squares, xIsNext: !xIsNext});
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}  
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const {xIsNext, squares} = this.state;
+    const winner = this.calculateWinner(squares);
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
